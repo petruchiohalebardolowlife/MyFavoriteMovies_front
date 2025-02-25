91c78042c084f4fde7fc41c510017c22ec6c3562
petruchiohalebardolowlife <levinyegor2000@gmail.com>
2025-02-25 21:56:01 +0300

    remove btn class

--- src/components/LanguageButton.tsx
+++ src/components/LanguageButton.tsx
@@ -9,7 +9,7 @@ function LanguageButton({ locale, changeLanguage }: LanguageButtonProps) {
     changeLanguage(locale);
   };
   return (
-    <button onClick={handleClick} className="btn">
+    <button onClick={handleClick} className="py-2 px-4 bg-gray-800 text-white rounded hover:bg-gray-600 disabled:bg-gray-400">
       {locale}
     </button>
   );
